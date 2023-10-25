d4030019a14e5666478ac114a0800b98d3216526
AKBARVE04 <118174073+AKBARVE04@users.noreply.github.com>
2023-10-25 18:17:56 +0530

    Added stack operations codes

---
+++ Stack_Operations.cpp
@@ -0,0 +1,28 @@
+// Insert elements
+// Delete elements
+// Display elements
+// Exit elements
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Stack
+{
+    int top;
+    public;
+    int a[10];
+    Stack()
+    {
+        top = -1;
+    }
+    //  declaring all the functions
+    void push(int(x));
+    int pop();
+}
+
+int
+main()
+{
+}
